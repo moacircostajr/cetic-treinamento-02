e3b9b9bc5a1535bcb3b24c88b96393be6e9c909c
Moacir Costa <moacircostajr@gmail.com>
2021-01-28 05:50:26 -0300

    react context

---
+++ moacir/3_-_React_Hooks_Forms_e_Context/todo/src/contexts/TodoContext.tsx
@@ -0,0 +1,11 @@
+import React, { createContext } from 'react'
+import { TodoContextType } from './TodoContextType'
+
+export const TodoContext = createContext<TodoContextType>(
+  {
+    todos: [],
+    addTodo: () => { },
+    removeTodo: () => { },
+    toggle: () => { }
+  }
+)
