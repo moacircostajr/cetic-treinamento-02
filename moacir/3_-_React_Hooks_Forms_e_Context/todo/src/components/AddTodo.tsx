ff8e5673142184dd82512ad3894d55510de70fd7
Moacir Costa <moacircostajr@gmail.com>
2021-01-28 07:51:55 -0300

    react forms

--- moacir/3_-_React_Hooks_Forms_e_Context/todo/src/components/AddTodo.tsx
+++ moacir/3_-_React_Hooks_Forms_e_Context/todo/src/components/AddTodo.tsx
@@ -1,15 +1,42 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { TodoContext } from '../contexts/TodoContext'
+import { useForm } from 'react-hook-form'
+import { yupResolver } from '@hookform/resolvers/yup'
+import * as yup from 'yup'
+import { TodoContextType } from '../contexts/TodoContextType'
 
-interface Props {
+const schema = yup.object().shape({
+  title: yup.string().required('Tarefa inválida')
+})
 
+interface AddTodoForm {
+  title: string
 }
 
-const AddTodo = (props: Props) => {
+const AddTodo = () => {
+  const { addTodo } = useContext<TodoContextType>(TodoContext)
+  const { register, handleSubmit, errors } = useForm({
+    resolver: yupResolver(schema)
+  })
+  const onSubmit = (data: AddTodoForm, e: any) => {
+    addTodo(data.title)
+    e.target.reset()
+    window.location.href = '/'
+  }
   return (
-    <div>
-      <h1>Novo Todo</h1>
-    </div>
+    <form onSubmit={handleSubmit<AddTodoForm>(onSubmit)}>
+      <h4>Nova tarefa</h4>
+      <div className="uk-margin uk-width-1-1">
+        <input type="text" name="title" id="title" placeholder="Nova tarefa..." className="uk-input" ref={register} />
+        <span><small><strong className="uk-text-danger">{errors.title?.message}</strong></small></span>
+      </div>
+      <div className="uk-width-1-1">
+        <button type="submit" className="uk-button uk-button-primary">Salvar</button>
+      </div>
+    </form>
   )
 }
 
 export default AddTodo
+
+// yarn add react-hook-form yup @types/yup @hookform/resolvers
